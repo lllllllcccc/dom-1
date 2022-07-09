8b118761da7c7d44f47d7f1087363535b8a790cd
datongtong <2784746424@qq.com>
2022-07-09 10:48:53 +0800

    作业

--- src/main.js
+++ src/main.js
@@ -1,1 +1,5 @@
-console.log("hi")
+const div = dom.find('.red', '#test')[0]
+console.log(div)
+dom.style(div, 'color', 'red')
+const divList = dom.find('.red')
+dom.each(divList, (n) => console.log(n))
